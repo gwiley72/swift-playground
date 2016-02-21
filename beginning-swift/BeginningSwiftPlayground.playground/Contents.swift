5bd452fb1d7fcf57f31250169eb5ae2675f9c549
gwiley72 <glennwiley@gmail.com>
2016-02-21 16:24:41 -0600

    beginning swift 2 - lesson 4 - functions

--- beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
+++ beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
@@ -1,5 +1,20 @@
-//: Playground - noun: a place where people can play
 
 import Cocoa
 
-var str = "Hello, playground"
+func calculateTipOnTotal(totalBill: Double, withRating rating: Int) -> Double {
+    let tipPercentage: Double
+
+    if rating == 5 {
+        tipPercentage = 0.25
+    } else if rating >= 3 {
+        tipPercentage = 0.15
+    } else {
+        tipPercentage = 0.10
+    }
+
+    return totalBill * tipPercentage
+}
+
+let restaurantTotal = 95.00
+let tip = calculateTipOnTotal(95.0, withRating: 3)
+restaurantTotal + tip
