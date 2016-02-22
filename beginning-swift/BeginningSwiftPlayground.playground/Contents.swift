80425493f51b61fccf0435e783736db569d882fa
gwiley72 <glennwiley@gmail.com>
2016-02-21 18:33:54 -0600

    tutorial 5 - optionals

--- beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
+++ beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
@@ -1,9 +1,9 @@
 
 import Cocoa
 
-func calculateTipOnTotal(totalBill: Double, withRating rating: Int) -> Double {
+func tipPercentageForRating(rating: Int) -> Double {
     let tipPercentage: Double
-
+    
     if rating == 5 {
         tipPercentage = 0.25
     } else if rating >= 3 {
@@ -11,10 +11,33 @@ func calculateTipOnTotal(totalBill: Double, withRating rating: Int) -> Double {
     } else {
         tipPercentage = 0.10
     }
+    
+    return tipPercentage
+}
 
+func calculateTipOnTotal(totalBill: Double, withRating rating: Int) -> Double {
+    let tipPercentage = tipPercentageForRating(rating)
+    
     return totalBill * tipPercentage
 }
 
-let restaurantTotal = 95.00
-let tip = calculateTipOnTotal(95.0, withRating: 3)
-restaurantTotal + tip
+func showTipAdvice(restaurantTotal restaurantTotal: Double, rating: Int, restaurantName: String?) {
+    let tip = calculateTipOnTotal(restaurantTotal, withRating: rating)
+    
+    if let restaurantName = restaurantName {
+        print("You went out to eat at \(restaurantName)")
+    }
+
+    print("On a bill of $\(restaurantTotal), you should leave a tip of $\(tip)")
+    print("That means a total of $\(restaurantTotal + tip)")
+}
+
+var restaurantName: String?
+
+restaurantName = "Burger Barn"
+
+showTipAdvice(restaurantTotal: 65, rating: 5, restaurantName: restaurantName)
+
+
+
+
