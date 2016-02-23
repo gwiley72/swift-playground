0672efb865bf37b2d02c7b62cbaf8bca428e9aec
gwiley72 <glennwiley@gmail.com>
2016-02-23 08:32:26 -0600

    add challenge for lesson 6

--- beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
+++ beginning-swift/BeginningSwiftPlayground.playground/Contents.swift
@@ -1,27 +1,39 @@
 import Cocoa
 
-func tipPercentageForRating(rating: Int) -> Double? {
-    guard rating >= 1 && rating <= 5 else {
-        return nil
+struct RestaurantBill {
+    let restaurantName: String
+    var totalBill: Double
+    var rating: Int
+    
+    func tipPercentage() -> Double? {
+        guard rating >= 1 && rating <= 5 else {
+            return nil
+        }
+        
+        let tipPercentage: Double
+        
+        if rating == 5 {
+            tipPercentage = 0.25
+        } else if rating >= 3 {
+            tipPercentage = 0.15
+        } else {
+            tipPercentage = 0.10
+        }
+        
+        return tipPercentage
     }
     
-    let tipPercentage: Double
-    
-    if rating == 5 {
-        tipPercentage = 0.25
-    } else if rating >= 3 {
-        tipPercentage = 0.15
-    } else {
-        tipPercentage = 0.10
+    func calculateTip() -> Double {
+        guard let tipPercentage = tipPercentage() else {
+            return totalBill
+        }
+        
+        return totalBill * tipPercentage
     }
-    
-    return tipPercentage
 }
 
-//func calculateTipOnTotal(totalBill: Double, withRating rating: Int) -> Double {
-//    guard let tipPercentage = tipPercentageForRating(rating) else {
-//        return totalBill
-//    }
-//    
-//    return totalBill
-//}
+let chickenCafeBill = RestaurantBill(restaurantName: "Chicken Cafe", totalBill: 85, rating: 5)
+
+
+
+chickenCafeBill.calculateTip()
